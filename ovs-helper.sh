c6d8713621eddbe946f8aaf17fcfb33fb42e50f4
Raul Olaya <raul.olaya@gmail.com>
2019-11-23 15:36:20 -0500

    Temporary changes to facilitate simple ovs deployment
    1. Updated homemade ovs-helper.sh (to setup deploment of simple ovs configuration as per: http://docs.openvswitch.org/en/latest/howto/qos/).
    2. Prevent build failure after introduction of ovs-helper.sh file into project (temporary fix, need to understand how to properly introduce a new file into project (if at all)).

--- ovs-helper.sh
+++ ovs-helper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#==================================================================================================================
+# 
+#==================================================================================================================
 ovs_override_kernel_modules()
 {
 	config_file="/etc/depmod.d/openvswitch.conf"
@@ -11,6 +14,9 @@ ovs_override_kernel_modules()
 	done
 }
 
+#==================================================================================================================
+# 
+#==================================================================================================================
 ovs_config_db()
 {
 	mkdir -p /usr/local/etc/openvswitch
@@ -18,6 +24,9 @@ ovs_config_db()
 }
 
 
+#==================================================================================================================
+# 
+#==================================================================================================================
 ovs_start_db_server()
 {
 	mkdir -p /usr/local/var/run/openvswitch
@@ -28,6 +37,59 @@ ovs_start_db_server()
 	    --certificate=db:Open_vSwitch,SSL,certificate \
 	    --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
 	    --pidfile --detach --log-file
+}
+
+#==================================================================================================================
+# 
+#==================================================================================================================
+ovs_deploy_test()
+{
+  # These commands are executed as "root" user (for now)
+  
+  export PATH=$PATH:/usr/local/share/openvswitch/scripts
+
+  ovs-ctl start
+
+  ovs-vsctl add-br br0
+  
+  ip link set br0 up
+
+  ovs-vsctl add-port br0 enp5s0
+  
+  ip addr del 192.168.1.206/24 dev enp5s0
+
+  dhclient br0
+
+
+  # tap port for VM1
+  ip tuntap add mode tap tap_port1
+
+  ip link set tap_port1 up
+
+  ovs-vsctl add-port br0 tap_port1
+
+  # tap port for VM2
+  ip tuntap add mode tap tap_port2
+
+  ip link set tap_port2 up
+
+  ovs-vsctl add-port br0 tap_port2
+}
+
+#==================================================================================================================
+# 
+#==================================================================================================================
+ovs_run_test()
+{
+  # These commands are executed as "root" user (for now)
+  
+  export PATH=$PATH:/usr/local/share/openvswitch/scripts
+
+  ip addr del 192.168.1.206/24 dev enp5s0
+  
+  ovs-ctl start
+
+  dhclient br0
 }
 
 
