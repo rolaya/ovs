e4bf05a559ae26e627b4ad6dcdd5f5159fc9f8d0
Raul Olaya <raul.olaya@gmail.com>
2019-11-25 14:33:38 -0500

    Temporary changes to facilitate simple ovs deployment
    1. Replaced tabs with spaces (to address "recipe for target 'check-tabs' failed").

--- ovs-helper.sh
+++ ovs-helper.sh
@@ -5,13 +5,13 @@
 #==================================================================================================================
 ovs_override_kernel_modules()
 {
-	config_file="/etc/depmod.d/openvswitch.conf"
+  config_file="/etc/depmod.d/openvswitch.conf"
 
-	for module in datapath/linux/*.ko; do
-		modname="$(basename ${module})"
-		echo "override ${modname%.ko} * extra" >> "$config_file"
-		echo "override ${modname%.ko} * weak-updates" >> "$config_file"
-	done
+  for module in datapath/linux/*.ko; do
+    modname="$(basename ${module})"
+    echo "override ${modname%.ko} * extra" >> "$config_file"
+    echo "override ${modname%.ko} * weak-updates" >> "$config_file"
+  done
 }
 
 #==================================================================================================================
@@ -19,8 +19,8 @@ ovs_override_kernel_modules()
 #==================================================================================================================
 ovs_config_db()
 {
-	mkdir -p /usr/local/etc/openvswitch
-	ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+  mkdir -p /usr/local/etc/openvswitch
+  ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
 }
 
 
@@ -29,14 +29,14 @@ ovs_config_db()
 #==================================================================================================================
 ovs_start_db_server()
 {
-	mkdir -p /usr/local/var/run/openvswitch
+  mkdir -p /usr/local/var/run/openvswitch
 
-	ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-	    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-	    --private-key=db:Open_vSwitch,SSL,private_key \
-	    --certificate=db:Open_vSwitch,SSL,certificate \
-	    --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
-	    --pidfile --detach --log-file
+  ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+    --private-key=db:Open_vSwitch,SSL,private_key \
+    --certificate=db:Open_vSwitch,SSL,certificate \
+    --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+    --pidfile --detach --log-file
 }
 
 #==================================================================================================================
