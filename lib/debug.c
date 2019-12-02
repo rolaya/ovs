6269671ff965ca290541a93cfbaadb377e37c9fb
Raul Olaya <raul.olaya@gmail.com>
2019-12-02 14:12:51 -0500

    Traffic shaping related changes
    1. Added function to log binary data (as asciihex).
    2. Added function to convert to string TCA_* types.
    2. Updated ovs helper scripts (added functions to install ovs, start/stop ovs test).

--- lib/debug.c
+++ lib/debug.c
@@ -1,8 +1,19 @@
+//==================================================================================================================
+//
+//==================================================================================================================
 #include <config.h>
 
+//==================================================================================================================
+//
+//==================================================================================================================
 #include "netdev-linux.h"
 #include "netdev-linux-private.h"
+#include "openvswitch/vlog.h"
+#include "debug.h"
 
+//==================================================================================================================
+//
+//==================================================================================================================
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -19,6 +30,12 @@
 #include <linux/mii.h>
 #include <linux/rtnetlink.h>
 
+/* Required for VLOG* logging */
+VLOG_DEFINE_THIS_MODULE(debug);
+
+//==================================================================================================================
+//
+//==================================================================================================================
 char* rtm_to_string(int code)
 {
   char *rtm_name;
@@ -264,6 +281,321 @@ char* rtm_to_string(int code)
 
   return rtm_name;
 }
+
+//==================================================================================================================
+//
+//==================================================================================================================
+char* tca_to_string(uint16_t type)
+{
+  char *tca_name;
+
+  switch(type)
+  {
+  case TCA_UNSPEC:
+    tca_name = "TCA_UNSPEC";
+    break;
+
+  case TCA_KIND:
+    tca_name = "TCA_KIND";
+    break;
+  
+  case TCA_OPTIONS:
+    tca_name = "TCA_OPTIONS";
+    break;
+  
+  case TCA_STATS:
+    tca_name = "TCA_STATS";
+    break;
+  
+  case TCA_XSTATS:
+    tca_name = "TCA_XSTATS";
+    break;
+  
+  case TCA_RATE:
+    tca_name = "TCA_RATE";
+    break;
+  
+  case TCA_FCNT:
+    tca_name = "TCA_FCNT";
+    break;
+  
+  case TCA_STATS2:
+    tca_name = "TCA_STATS2";
+    break;
+  
+  case TCA_STAB:
+    tca_name = "TCA_STAB";
+    break;
+  
+  case TCA_PAD:
+    tca_name = "TCA_PAD";
+    break;
+  
+/*  case TCA_DUMP_INVISIBLE:
+    tca_name = "TCA_DUMP_INVISIBLE";
+    break;*/
+  
+/*  case TCA_CHAIN:
+    tca_name = "TCA_CHAIN";
+    break;*/
+ 
+/* case TCA_INGRESS_BLOCK:
+    tca_name = "TCA_INGRESS_BLOCK";
+    break;*/
+  
+/*  case TCA_EGRESS_BLOCK:
+    tca_name = "TCA_EGRESS_BLOCK";
+    break;*/
+  
+  default:
+    tca_name = "TCA_???";
+    break;
+  }
+
+  return tca_name;
+}
+
+
+#define PRINT_FORMAT_uint8_t              0x00000001
+#define FORMAT_INFO_INCLUDE_HEX_PREFIX  0x00000002
+#define FORMAT_INFO_INCLUDE_DECODING    0x00000004
+
+//==================================================================================================================
+//
+//==================================================================================================================
+void Bin2Hex(char *dest, int sizeof_dest, const uint8_t *src, int len, uint32_t PrintFlags)
+{
+  int n;
+  int k;
+  char* pDestination;
+  int bytes_converted;
+  int remaining_space;
+  int iNumberOfPadSpaces;
+  const uint8_t* pSource;
+  bool bDisplayPrintable;
+  bool bDisplayHexIndicator;
+
+  /* Display printable data */
+  bDisplayPrintable = (PrintFlags & FORMAT_INFO_INCLUDE_DECODING);
+
+  /* Do not prepend every ascii-hex byte with 0x */
+  bDisplayHexIndicator = (PrintFlags & FORMAT_INFO_INCLUDE_HEX_PREFIX);
+
+  /* Save pointer to first byte of destination buffer */
+  pDestination = dest;
+
+  /* Keep track of number of bytes converted */
+  bytes_converted = 0;
+
+  /* Keep track of space remaining in destination buffer */
+  remaining_space = sizeof_dest;
+
+  /* Save pointer to source data, we'll use it to show printable data */
+  pSource = src;
+
+  iNumberOfPadSpaces = 8;
+
+  /* Convert binary character to ASCII hex */
+  while (len--)
+  {
+    if (bDisplayHexIndicator)
+    {
+      /* Make sure there is space for a full ascii-hex character (e.g. 0x30), line termination (CR/LF) and NULL termination. */
+      if (remaining_space < 7)
+      {
+        break;
+      }
+
+      /* Append 0x to every byte */
+      *(dest++) = '0';
+      *(dest++) = 'x';
+    }
+    else
+    {
+      /* Make sure there is space for a full ascii-hex character (e.g. 30), line termination (CR/LF) and NULL termination. */
+      if (remaining_space < 5)
+      {
+        break;
+      }
+    }
+
+    /* Process upper nibble */
+    n = (*(src) >> 4) & 0x0f;
+
+    if (n <= 9)
+    {
+      n += '0';
+    }
+    else
+    {
+      n += 'a' - 10;
+    }
+
+    *(dest++) = (char)n;
+
+    /* Process lower nibble*/
+    n = *(src++) & 0x0f;
+
+    if (n <= 9)
+    {
+      n += '0';
+    }
+    else
+    {
+      n += 'a' - 10;
+    }
+
+    *(dest++) = (char)n;
+
+    /* Update number of binary bytes converted to ascii-hex */
+    bytes_converted++;
+
+    /* More than one byte remaining to be converted? */
+    if (len >= 1)
+    {
+      /* Print 8 ascii-hex per line */
+      if (bytes_converted % 8 == 0)
+      {
+        iNumberOfPadSpaces = 8;
+
+        /* Display printable characters? */
+        if (bDisplayPrintable)
+        {
+          /* Add spaces between ascii-hex data and their printable values */
+          *(dest++) = ' ';
+          *(dest++) = ' ';
+          *(dest++) = ' ';
+          *(dest++) = ' ';
+
+          while (pSource < src)
+          {
+            /* Printable character? */
+            if ((*pSource > 0x1f) && (*pSource < 0x7f))
+            {
+              *(dest++) = *pSource;
+            }
+            else
+            {
+              /* Non printable character */
+              *(dest++) = '.';
+            }
+
+            /* Point to next data byte to print */
+            pSource++;
+          }
+        }
+      }
+      else
+      {
+        /* Add space between bytes */
+        *(dest++) = ' ';
+
+        iNumberOfPadSpaces--;
+      }
+    }
+
+    /* Get number remaining in destination buffer */
+    remaining_space = sizeof_dest - (int)(dest - pDestination);
+  }
+
+  /* Display printable characters? */
+  if (bDisplayPrintable)
+  {
+    /* Calculate number of spaces (padding) */
+    iNumberOfPadSpaces = (iNumberOfPadSpaces - 1) * 3;
+
+    /* Perform the padding */
+    for (k = 0; k < iNumberOfPadSpaces; k++)
+    {
+      *(dest++) = ' ';
+    }
+
+    /* Add spaces between ascii-hex data and their printable values */
+    *(dest++) = ' ';
+    *(dest++) = ' ';
+    *(dest++) = ' ';
+    *(dest++) = ' ';
+
+    while (pSource < src)
+    {
+      /* Printable character? */
+      if ((*pSource > 0x1f) && (*pSource < 0x7f))
+      {
+        *(dest++) = *pSource;
+      }
+      else
+      {
+        /* Non printable character */
+        *(dest++) = '.';
+      }
+
+      /* Point to next data byte to print */
+      pSource++;
+    }
+  }
+}
+
+//==================================================================================================================
+//
+//==================================================================================================================
+void LogBuffer(char *Message, const uint8_t* pData, int DataSize)
+{
+  int iOffset;
+  int LenRemaining;
+  char buffer[512];
+  uint32_t PrintFlags;
+  const uint8_t* pBuffer;
+  int NumberOfByestToPrint;
+  const uint8_t *pSourceBuffer;
+  const int MaxBytesPerIteration = 8;
+
+  /* Display message and number of bytes in buffer */
+  VLOG_INFO("%s: [%d] bytes", Message, DataSize);
+
+  /* Init print flags */
+  PrintFlags = 0x00000000;
+
+  /* Insure pData is not NULL */
+  if (pData)
+  {
+    /* Offset into source data to print/log */
+    iOffset = 0;
+    pSourceBuffer = pData;
+    pBuffer = pSourceBuffer;
+
+    /* Number of bytes remaining to print/log */
+    LenRemaining = DataSize;
+
+    PrintFlags |= FORMAT_INFO_INCLUDE_DECODING;
+
+    while (LenRemaining > 0)
+    {
+      /* Convert at most 64 binary bytes at one time */
+      NumberOfByestToPrint = (LenRemaining > MaxBytesPerIteration ? MaxBytesPerIteration : LenRemaining);
+
+      /* Used for clarity to print the buffer address */
+      pBuffer = &pSourceBuffer[iOffset];
+
+      /* Clear destination buffer */
+      memset(buffer, 0x00, sizeof(buffer));
+
+      /* Convert to hex */
+      Bin2Hex(buffer, sizeof(buffer), (uint8_t*)&pSourceBuffer[iOffset], NumberOfByestToPrint, PrintFlags);
+
+      VLOG_INFO("0x%p: [%s]", pBuffer, buffer);
+
+      /* Print address and data (right now we print 8 ascii-hex bytes at one time) */
+      //DEBUG((DebugMask, "0x%p: [%s]\n", pBuffer, buffer));
+
+      /* Update offset into source buffer */
+      iOffset += MaxBytesPerIteration;
+
+      /* Update number of bytes remaining to print */
+      LenRemaining -= NumberOfByestToPrint;
+    }
+  }
+}
+
 
 
 
--- lib/debug.h
+++ lib/debug.h
@@ -1,1 +1,7 @@
 char* rtm_to_string(int code);
+char* tca_to_string(uint16_t type);
+
+void Bin2Hex(char *dest, int sizeof_dest, const uint8_t *src, int len, uint32_t PrintFlags);
+void LogBuffer(char *Message, const uint8_t* pData, int DataSize);
+
+
